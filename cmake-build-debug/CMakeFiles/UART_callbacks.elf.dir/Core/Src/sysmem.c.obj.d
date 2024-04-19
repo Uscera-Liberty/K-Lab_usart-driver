e43c5004669f5944b371e0878c933209a251b044
Uscera <maslyayeva.yuliya@bk.ru>
2024-04-19 16:42:14 +0300

    added callback for receive

---
+++ cmake-build-debug/CMakeFiles/UART_callbacks.elf.dir/Core/Src/sysmem.c.obj.d
@@ -0,0 +1,21 @@
+CMakeFiles/UART_callbacks.elf.dir/Core/Src/sysmem.c.obj: \
+ /home/kovoya_mint/STMproject/UART_callbacks/Core/Src/sysmem.c \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/errno.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/errno.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/reent.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/_ansi.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/newlib.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/config.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/machine/ieeefp.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/cdefs.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/_types.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/machine/_types.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/lock.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/stdint.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/_stdint.h
---
+++ cmake-build-debug/CMakeFiles/UART_callbacks.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c.obj.d
@@ -0,0 +1,37 @@
+CMakeFiles/UART_callbacks.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c.obj: \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Core/Inc/stm32f0xx_hal_conf.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/CMSIS/Include/core_cm0.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/stdint.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/arm-none-eabi/include/sys/_stdint.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/kovoya_mint/toolchain_arm/arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_exti.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h \
+ /home/kovoya_mint/STMproject/UART_callbacks/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h
